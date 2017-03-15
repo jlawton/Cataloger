01a2711777c367059393bdeb861216e53b9b1ad6
James Lawton <james.lawton@gmail.com>
2017-03-14 21:49:26 -0700

    Initial Commit

---
+++ Cataloger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cataloger
+//
+//  Created by James Lawton on 3/14/17.
+//  Copyright © 2017 James Lawton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
